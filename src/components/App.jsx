34a8d8dbc4b320db5627ec2771e3733f20689fcd
Anastasiia <pavlishinka12@icloud.com>
2022-12-16 15:09:22 +0200

    do refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,47 +1,45 @@
-import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { add, remove } from '../Redux/contaSlise';
+import { filter } from 'Redux/filterSlise';
+
 import { Form } from './FormApp';
 import { ContactList } from './ContactList';
 import { Filter } from './Filter';
 
 export const App = () => {
-  const [contacts, setContacts] = useState(()=> {
-    const contactStorage = localStorage.getItem('contacts');
-    const contactParse = JSON.parse(contactStorage);
-    return contactParse || []
-  });
-  const [filter, setFilter] = useState('');
-
-  useEffect(() => {
-    if (contacts.length === 0) {
-      return;
-    }
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
+  const contacts = useSelector(state => state.contacts);
+  const filterVal = useSelector(state => state.filter.value);
+  const dispatch = useDispatch();
 
   const filterInput = event => {
-    setFilter(event.target.value);
+    dispatch(filter(event.target.value));
   };
 
   const onFilterSearch = () => {
-    return contacts.filter(contact => {
-      return contact.name.toLowerCase().includes(filter.toLowerCase());
-    });
+    const normalizedFilter = filterVal.toLowerCase();
+    if (contacts !== []) {
+      const filtContact = contacts.filter(contact =>
+        contact.name.toLowerCase().includes(normalizedFilter)
+      );
+      return filtContact;
+    }
   };
 
   const deleteUser = userId => {
-    setContacts([...contacts.filter(contact => contact.number !== userId)]);
-    // this.setState( prevState => ({
-    //   contacts: prevState.contacts.filter(contact => contact.number !== userId)
-    // }))
+    dispatch(remove(userId));
   };
 
-  const dataFormSubmit = data => {
+  const dataFormSubmit = ({ ...data }) => {
     const searchName = contacts.map(contact => contact.name);
+    const searchNumber = contacts.map(contact => contact.number);
+
     if (searchName.includes(data.name)) {
-      alert('Не получится добавить такое имя:(');
-    } else {
-      setContacts([data, ...contacts]);
+      alert(`${data.name} уже есть в Вашем списке контактов`);
     }
+    if (searchNumber.includes(data.number)) {
+      alert(`В Вашем списке контактов уже есть номер ${data.number}`);
+    }
+    dispatch(add(data));
   };
 
   return (
@@ -49,7 +47,7 @@ export const App = () => {
       <h1>Phonebook</h1>
       <Form onSubmit={dataFormSubmit} />
       <h2>Contacts</h2>
-      <Filter onChange={filterInput} value={filter} />
+      <Filter onChange={filterInput} value={filterVal} />
       <ContactList listsContact={onFilterSearch()} onDelete={deleteUser} />
     </div>
   );
